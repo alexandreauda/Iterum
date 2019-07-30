1212c052f6cfbfe9110babc0d469ac6a9913d6a9
alexandreauda <alexandreauda@yahoo.fr>
2019-07-30 02:15:58 +0200

    [UPDATE] Implement prototype for Custom Multiplier time speed

--- GlobalMacroRecorder/EventSettingForm.Designer.cs
+++ GlobalMacroRecorder/EventSettingForm.Designer.cs
@@ -45,9 +45,18 @@ namespace GlobalMacroRecorder
             this.radioButtonNormalSpeed = new System.Windows.Forms.RadioButton();
             this.cancelButton = new System.Windows.Forms.Button();
             this.okButton = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.groupBoxGlobal.SuspendLayout();
             this.ScrollPanel.SuspendLayout();
             this.groupBoxCustomUniformSpeed.SuspendLayout();
+            this.groupBoxCustomMultiplierSpeed.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBoxGlobal
@@ -119,12 +128,12 @@ namespace GlobalMacroRecorder
             // 
             // textBoxUniformSpeed
             // 
-            this.textBoxUniformSpeed.Location = new System.Drawing.Point(143, 26);
+            this.textBoxUniformSpeed.Location = new System.Drawing.Point(137, 29);
             this.textBoxUniformSpeed.Name = "textBoxUniformSpeed";
             this.textBoxUniformSpeed.Size = new System.Drawing.Size(100, 20);
             this.textBoxUniformSpeed.TabIndex = 0;
             this.textBoxUniformSpeed.Text = "0";
-            this.textBoxUniformSpeed.KeyDown += new KeyEventHandler(this.textBoxUniformSpeed_enterInput);
+            this.textBoxUniformSpeed.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxUniformSpeed_enterInput);
             // 
             // radioButtonCustomUniformSpeed
             // 
@@ -139,6 +148,12 @@ namespace GlobalMacroRecorder
             // 
             // groupBoxCustomMultiplierSpeed
             // 
+            this.groupBoxCustomMultiplierSpeed.Controls.Add(this.numericUpDown1);
+            this.groupBoxCustomMultiplierSpeed.Controls.Add(this.label4);
+            this.groupBoxCustomMultiplierSpeed.Controls.Add(this.label3);
+            this.groupBoxCustomMultiplierSpeed.Controls.Add(this.label2);
+            this.groupBoxCustomMultiplierSpeed.Controls.Add(this.label1);
+            this.groupBoxCustomMultiplierSpeed.Controls.Add(this.trackBar1);
             this.groupBoxCustomMultiplierSpeed.Location = new System.Drawing.Point(23, 90);
             this.groupBoxCustomMultiplierSpeed.Name = "groupBoxCustomMultiplierSpeed";
             this.groupBoxCustomMultiplierSpeed.Size = new System.Drawing.Size(318, 76);
@@ -197,6 +212,69 @@ namespace GlobalMacroRecorder
             this.okButton.UseVisualStyleBackColor = true;
             this.okButton.Click += new System.EventHandler(this.okButton_Click);
             // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 1;
+            this.trackBar1.Location = new System.Drawing.Point(5, 13);
+            this.trackBar1.Maximum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(45, 57);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Value = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(38, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "/";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(38, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "x";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(50, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Multiply time speed";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(50, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Divide time speed";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(185, 32);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
             // EventSettingForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -213,6 +291,10 @@ namespace GlobalMacroRecorder
             this.ScrollPanel.PerformLayout();
             this.groupBoxCustomUniformSpeed.ResumeLayout(false);
             this.groupBoxCustomUniformSpeed.PerformLayout();
+            this.groupBoxCustomMultiplierSpeed.ResumeLayout(false);
+            this.groupBoxCustomMultiplierSpeed.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -232,6 +314,12 @@ namespace GlobalMacroRecorder
         private System.Windows.Forms.RadioButton radioButtonNormalSpeed;
         private System.Windows.Forms.TextBox textBoxUniformSpeed;
         private System.Windows.Forms.Label labelCustomUniformSpeed;
+        private NumericUpDown numericUpDown1;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private TrackBar trackBar1;
     }
 }
 
